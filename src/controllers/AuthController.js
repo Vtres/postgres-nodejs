7c988c399ab77d00b4b1b90d4c68c47ed9024ab7
Vtres <viniciust98@outlook.com>
2021-10-06 09:38:46 -0300

    :tada: Implementing User Registration Controller

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -18,6 +18,7 @@ AuthController.post('/signin', async (req, res) => {
 
     try {
         const user = await ClientService.findUserByEmail(email)
+        console.log(user)
         const passwordValidated = await ClientService.validatePassword(senha, user.senha)
         if (!passwordValidated) {
             return res.status(401).json({ error: 'Senha inválida' })
@@ -61,6 +62,40 @@ AuthController.post('/check-token', async (req, res) => {
     } catch (error) {
         return res.status(500).json({error: error})
     }
+})
+
+AuthController.post('/addUser', async (req, res) => {
+    const {name, surname,email,senha,active} = req.body
+    const errors = []
+    if(!name){
+        errors.push({error:'Nome está em branco'})
+    }
+    if(!surname ){
+        errors.push({error:"Sobrenome está em branco"})
+    }
+    if(!email){
+        errors.push({error:"Email está em branco"})
+    }
+    if(!email.match(/\S+@\S+\.\S+/)){
+        errors.push({error:"Email inválido"})
+    }
+    if(!senha){
+        errors.push({error:"Senha está em branco"})
+    }
+
+    if(errors.length > 0){
+        return res.status(400).json(errors)
+    }
+    try {
+        const emailExist = await ClientService.existsEmail(email)
+        if(emailExist){
+            return res.status(409).json({message: `Este email já esta em uso`})
+        }else{
+             res.status(201).json(await ClientService.store({name, surname,email,senha,active}))
+        }
+     } catch (error) {
+         res.status(500).json({error: 'ClientService.existsEmail is not working'})
+     }
 })
 
 module.exports = AuthController
